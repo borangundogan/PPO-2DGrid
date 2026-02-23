8bdf7dbd1f957042ce11e53560c2e71b02a24359
Boran Gündoğan <75067510+borangundogan@users.noreply.github.com>
2026-02-23 20:48:36 +0100

    remove stuck utils

--- fomaml/utils.py
+++ fomaml/utils.py
@@ -1,18 +1,11 @@
-# fomaml/utils.py
-
 import numpy as np
 import torch
 
-import numpy as np
-import torch
-
-def evaluate_episode(env, policy, device, max_steps=100, deterministic=False):
+def evaluate_episode(env, policy, device, max_steps=256, deterministic=False):
     obs, _ = env.reset()    
     total_reward = 0
     done = False
     steps = 0
-    
-    position_history = []
     
     while not done and steps < max_steps:
         obs_np = np.array(obs)
@@ -21,20 +14,8 @@ def evaluate_episode(env, policy, device, max_steps=100, deterministic=False):
         else:
             obs_t = torch.tensor(obs_np, dtype=torch.float32, device=device).view(1, -1) 
             
-        current_pos = tuple(env.unwrapped.agent_pos)
-        position_history.append(current_pos)
-        
-        if len(position_history) > 20:
-            position_history.pop(0)
-            
-        is_stuck = len(position_history) == 20 and len(set(position_history)) <= 2
-            
         with torch.no_grad():
-            if is_stuck:
-                action, _, _ = policy.act(obs_t, deterministic=False, epsilon = 0.1)
-                position_history.clear()
-            else:
-                action, _, _ = policy.act(obs_t, deterministic=deterministic)
+            action, _, _ = policy.act(obs_t, deterministic=deterministic, epsilon=0.1)
             
         obs, reward, terminated, truncated, _ = env.step(action.item())
         total_reward += reward
